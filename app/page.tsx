8279cd8f505516be770a12f5a5f7ba80e318f38f
Ccreature09 <gabriellikov@gmail.com>
2023-11-06 21:15:30 +0200

    Responsive maybe

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import React from "react";
 import { Navbar } from "@/components/functional/navbar";
 import { Footer } from "@/components/functional/footer";
 
@@ -8,10 +8,10 @@ export default function Home() {
       <Navbar />
       <section className="bg-blue-500 p-8 text-white">
         <div className="container mx-auto">
-          <h1 className="text-6xl font-bold text-center mb-4">
+          <h1 className="text-4xl md:text-6xl font-bold text-center mb-4">
             Добре дошли в Chemistry Corner - Вашият Портал към Света на Химията!
           </h1>
-          <p className="text-lg text-center">
+          <p className="text-base md:text-lg text-center">
             Интересувате се от елементите, които съставят нашия свят или сте
             зачаровани от мистериите на химичните реакции? Chemistry Corner е
             вашият окончателен път към разгадаване на завладяващия свят на
@@ -23,11 +23,12 @@ export default function Home() {
       <section className="bg-white py-12">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {/* Blog Section */}
             <div className="p-4">
-              <h2 className="text-4xl font-semibold text-blue-500 mb-4">
+              <h2 className="text-4xl md:text-3xl lg:text-4xl font-semibold text-blue-500 mb-4">
                 Блог
               </h2>
-              <p>
+              <p className="text-sm md:text-base">
                 Заплъвайте в нашата колекция от информативни и увлекателни
                 статии, които обхващат широка гама химични теми. От основи до
                 напреднали концепции, блогът ни предоставя ценни насоки за
@@ -37,10 +38,10 @@ export default function Home() {
 
             {/* Comics Section */}
             <div className="p-4">
-              <h2 className="text-4xl font-semibold text-blue-500 mb-4">
+              <h2 className="text-4xl md:text-3xl lg:text-4xl font-semibold text-blue-500 mb-4">
                 Комикси
               </h2>
-              <p>
+              <p className="text-sm md:text-base">
                 Изследвайте по-леката страна на химията с нашия раздел с
                 комикси. Тези забавни и образователни комикси правят ученето на
                 химия лесно и ви подаряват усмивка.
@@ -49,10 +50,10 @@ export default function Home() {
 
             {/* Fun Problems Section */}
             <div className="p-4">
-              <h2 className="text-4xl font-semibold text-blue-500 mb-4">
+              <h2 className="text-4xl md:text-3xl lg:text-4xl font-semibold text-blue-500 mb-4">
                 Забавни Задачи
               </h2>
-              <p>
+              <p className="text-sm md:text-base">
                 Предизвикайте себе си с избор от забавни и предизвикателни
                 химични задачи. Заточете уменията си за решаване на задачи, като
                 си набавите доза забавление.
@@ -61,10 +62,10 @@ export default function Home() {
 
             {/* Tests Section */}
             <div className="p-4">
-              <h2 className="text-4xl font-semibold text-blue-500 mb-4">
+              <h2 className="text-4xl md:text-3xl lg:text-4xl font-semibold text-blue-500 mb-4">
                 Тестове
               </h2>
-              <p>
+              <p className="text-sm md:text-base">
                 Поставете знанията си на изпитание с нашите химични тестове и
                 викторини. Независимо дали сте ученик, който се подготвя за
                 изпити или просто искате да разширите познанията си, нашите
@@ -74,10 +75,10 @@ export default function Home() {
 
             {/* Periodic Table Section */}
             <div className="p-4">
-              <h2 className="text-4xl font-semibold text-blue-500 mb-4">
+              <h2 className="text-4xl md:text-3xl lg:text-4xl font-semibold text-blue-500 mb-4">
                 Периодична Таблица
               </h2>
-              <p>
+              <p className="text-sm md:text-base">
                 Открийте периодичната таблица като никога досега. Нашата
                 интерактивна периодична таблица предоставя подробна информация
                 за всеки химичен елемент. От атомния номер до свойствата и
@@ -87,7 +88,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <Footer></Footer>
+      <Footer />
     </>
   );
 }
