dbee88263e91b0c9194f63154493840b95ecb4a6
Ccreature09 <gabriellikov@gmail.com>
2023-11-09 00:50:56 +0200

    removed seconds from publish time

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -61,7 +61,13 @@ export default function Page() {
   }, []);
   const formatDate = (timestamp: Timestamp) => {
     const date = new Date(timestamp.seconds * 1000);
-    return date.toLocaleString();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Month is 0-based, so add 1
+    const day = date.getDate().toString().padStart(2, "0");
+    const hour = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+
+    return `${year}-${month}-${day} ${hour}:${minutes}`;
   };
   return (
     <>
--- components/functional/blogmod.tsx
+++ components/functional/blogmod.tsx
@@ -94,7 +94,13 @@ export const Blogmod = () => {
 
   const formatDate = (timestamp: Timestamp) => {
     const date = new Date(timestamp.seconds * 1000);
-    return date.toLocaleString();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Month is 0-based, so add 1
+    const day = date.getDate().toString().padStart(2, "0");
+    const hour = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+
+    return `${year}-${month}-${day} ${hour}:${minutes}`;
   };
 
   return (
