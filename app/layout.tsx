4d9096463d95d81a858ba50f17ab4ecea1d4a076
Ccreature09 <gabriellikov@gmail.com>
2023-11-10 23:25:26 +0200

    Tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Prodanova's Chemistry Corner",
+  title: "Chem Magic",
   description: "Chemistry Learning App",
   manifest: "/manifest.webmanifest",
 };
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
