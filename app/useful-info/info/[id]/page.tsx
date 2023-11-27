cb0409db0278e3de3736d82fb721dcb68e994dc3
Ccreature09 <gabriellikov@gmail.com>
2023-11-28 00:05:59 +0200

    use client

--- app/useful-info/info/[id]/page.tsx
+++ app/useful-info/info/[id]/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import { collection, query, getDocs, where } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
